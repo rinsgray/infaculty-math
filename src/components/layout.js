60799d7e525979c51bdacba9be6e8c1e10cf8359
rinsgray <batyamaxno@gmail.com>
2022-01-27 23:12:11 +0400

    Updated top menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,20 +2,32 @@ import * as React from "react"
 import { Link } from "gatsby"
 import * as layoutStyles from "./layout.module.css"
 import Header from "./header"
-
+import RowBlock from "./twoColumnBlock"
 
 export default function Layout(props) {
+  function isActive( {isCurrent} ) {
+    return isCurrent ? {className: "layoutStyles.active"} : null
+  }
   return (
     <div>
+    <RowBlock
+    left =
+    <div class={layoutStyles.topmenu}>
+    <Header headerText="infacultyMath" />
+    </div>
+    right =
     <div className={layoutStyles.topmenu}>
     <ul>
-    <li><Header headerText="infacultyMath" /></li>
-    <li><Link to = '/articles/'><p>Статьи</p></Link></li>
-    <li><Link to = '/tasks/'><p>Сборник задач</p></Link></li>
-    <li><Link to = '/schedule/'><p>Занятия</p></Link></li>
-    <li><Link to = '/materials/'><p>Материалы</p></Link></li>
+
+    <li><Link to = '/materials/' getProps={isActive}><p>Материалы</p></Link></li>
+    <li><Link to = '/schedule/' getProps={isActive}><p>Занятия</p></Link></li>
+    <li><Link to = '/tasks/' getProps={isActive}><p>Сборник задач</p></Link></li>
+    <li><Link to = '/articles/' getProps={isActive}><p>Статьи</p></Link></li>
+
+
     </ul>
     </div>
+    />
     <div className={layoutStyles.children}>
       {props.children}
     </div>
--- src/pages/tasks.js
+++ src/pages/tasks.js
@@ -1,15 +1,48 @@
 // Step 1: Import React
 import * as React from 'react'
 import  Layout  from '../components/layout'
+import  RowBlock  from '../components/twoColumnBlock'
+import  InsideBlock  from '../components/insideBlock'
 
 export default function Home(){
   return(
     <Layout header='Сборник задач'>
-      <div>
-        Тут блоки по ЕГЭшкам и ОГЭшкам
+    <RowBlock
+      blockHeader = 'ЕГЭ'
+      left = <div>
+        <InsideBlock inheader='Первая часть'>
+        <ul>
+          <li>-</li>
+          <li>Вероятности</li>
+        </ul>
+        </InsideBlock>
+
       </div>
+      right = <div>
+      <InsideBlock inheader='Вторая часть'>
+      <ul>
+        <li> Уравнения </li>
+        <li> Стереометрия </li>
+        <li> Неравенства </li>
+        <li> Экономика
+        <ul>
+          <li>Вклады-кредиты
+            <ul>
+              <li>Подбираемые выплаты</li>
+            </ul>
+          </li>
+        </ul>
+        </li>
+      </ul>
+      </InsideBlock>
+      </div>
+    />
 
-
+    <RowBlock
+      blockHeader = 'Блок№2'
+      left = <InsideBlock> </InsideBlock>
+      right = <InsideBlock> </InsideBlock>
+    />
     </Layout>
   )
 }
